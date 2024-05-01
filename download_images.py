47924b22a2204100c0003e73407aa609f4326f92
ANDYTIEN1 <andytien0411@gmail.com>
2024-05-01 19:43:15 +0800

    yolov8-project

---
+++ download_images.py
@@ -0,0 +1,9 @@
+from simple_image_download import simple_image_download as simp
+
+response=simp.simple_image_download
+
+keywords=["lunch"]
+
+for kw in keywords:
+    response().download(kw,200)
+    
